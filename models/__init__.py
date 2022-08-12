2533e20c7dea6e640f025a1f7af1e480ab6d2015
Xiaojian Zhong <xiaojian.zhong@outlook.com>
2022-08-12 17:41:08 +0800

    add models and experiments

--- models/__init__.py
+++ models/__init__.py
@@ -48,22 +48,32 @@ def build_model():
         model = timm.create_model('mixnet_xl', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'mixnet-xxl':
         model = timm.create_model('mixnet_xxl', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
+    elif CFG.MODEL.NAME == 'mobilenet-v2':
+        model = timm.create_model('mobilenetv2_100', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'mobilenet-v3-large':
         model = timm.create_model('mobilenetv3_large_100', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'inception-v3':
         model = timm.create_model('inception_v3', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'inception-v4':
         model = timm.create_model('inception_v4', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
+    elif CFG.MODEL.NAME == 'inception-resnet-v2':
+        model = timm.create_model('inception_resnet_v2', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'xception-65':
         model = timm.create_model('xception65', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'xception-71':
         model = timm.create_model('xception71', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'nasnet-large':
         model = timm.create_model('nasnetalarge', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
+    elif CFG.MODEL.NAME == 'mnasnet-100':
+        model = timm.create_model('mnasnet_100', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'hrnet-18':
         model = timm.create_model('hrnet_w18', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     elif CFG.MODEL.NAME == 'densenet-121':
         model = timm.create_model('densenet121', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
+    elif CFG.MODEL.NAME == 'dla-102':
+        model = timm.create_model('dla102', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
+    elif CFG.MODEL.NAME == 'fbnet-v3-g':
+        model = timm.create_model('fbnetv3_g', pretrained=True, num_classes=CFG.DATASET.NUM_CLASSES)
     else:
         raise NotImplementedError(f'invalid model: {CFG.MODEL.NAME}')
 
